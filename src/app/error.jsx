5c114489cb8fdb0e5ddaad0cc82f1a69c4610c38
Raphael-Soares <soaresphaell@gmail.com>
2023-05-26 08:47:31 -0300

    Created Loading Spinner

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -12,7 +12,10 @@ export default function Error({ error, reset }) {
     return (
         <div className="text-center mt-10">
             <h1>Something went wrong: {error.message}</h1>
-            <button className="hover:text-amber-600 bg-gray-100 rounded " onClick={() => reset()}>
+            <button
+                className="hover:text-amber-600 bg-gray-300 rounded px-4 py-2  "
+                onClick={() => reset()}
+            >
                 Reset
             </button>
         </div>
